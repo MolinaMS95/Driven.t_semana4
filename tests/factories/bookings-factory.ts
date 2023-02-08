bdcd6c223b105e71de4d2cf2b72c2a6f2b13935c
MolinaMS95 <matheusmolina95@gmail.com>
2023-02-08 11:57:38 -0300

    test: add coverage

--- tests/factories/bookings-factory.ts
+++ tests/factories/bookings-factory.ts
@@ -1,5 +1,4 @@
 import { prisma } from "@/config";
-import faker from "@faker-js/faker";
 
 export async function createBooking(userId: number, roomId: number) {
   return await prisma.booking.create({
